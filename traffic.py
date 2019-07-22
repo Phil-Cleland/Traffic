0beab55c4ab33a4559dab38134f199171ad392f1
PhilemonCleland <philemoncleland@gmail.com>
2019-07-22 15:38:19 +0000

    adding button to the traffic light

--- traffic.py
+++ traffic.py
@@ -1,29 +1,44 @@
-from gpiozero import LED, Button
+from gpiozero import LED, Button, Buzzer
 from time import sleep
 from signal import pause
   
-#buzzer = Buzzer(15)  
-#button = Button()  
+buzzer = Buzzer(2)  
+button = Button(21)  
 red = LED(17)
 yellow = LED(11)
 green = LED(26)
 
 
-while True:  
-           #print("Wait")
+while True:
+                       
+            red.on()
+            print("Stop")
+            sleep(5)
+            red.off()
+            sleep(1)
+            yellow.on()
+            print("Get ready")
+            sleep(3)
+            yellow.off()
+            sleep(1)
+            green.on()
+            print("Go go go")
+            sleep(7)
+            green.off()
+            sleep(1)
+            if button.is_pressed:
+                buzzer.on()
+                sleep(1)
+                red.on()
+                print("STOP")
+                sleep(5)
+                red.off()
+                sleep(1)
+            
+           #buzzer.on()
+           #sleep(1)
+           #buzzer.off()
+           #sleep(1)
+           #print("beep")
            
-           red.on()
-           print("Stop")
-           sleep(7)
-           red.off()
-           sleep(1)
-           yellow.on()
-           print("Get ready")
-           sleep(4)
-           yellow.off()
-           sleep(1)
-           green.on()
-           print("Go go go")
-           sleep(8)
-           green.off()
-           sleep(1)
+           
